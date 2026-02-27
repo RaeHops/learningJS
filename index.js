a38851255a4a7eb3b210e68ac71a144fd74850b5
RaeHops <Raegan_hopkins26@milton.edu>
2026-02-27 11:26:29 -0500

    started learning HTML, learned how to use a new way to do if statements

---
+++ index.js
@@ -0,0 +1,28 @@
+console.log("Hello World");
+
+//#1 - Show if a randomly generated number is divisible by 7 and 9
+let x = 7;
+let y = 9;
+let z = 63//Math.floor(Math.random() * 21);
+
+console.log(z);
+
+let a = z % x;
+let b = z % y;
+
+if (a > 0){
+    console.log("false");
+} else if (b > 0){
+    console.log("false");
+} else {
+    console.log("true");
+}
+
+//#2 - Use ternary operator to decide if the weather is too cold, too hot, or perfect
+
+let weather = Math.floor(Math.random() * 134);
+console.log(weather)
+let text = (weather > 60) ? (weather < 90) ? "perfect" : "hot" : "Cold";
+console.log(text)
+
+//#3 - 
